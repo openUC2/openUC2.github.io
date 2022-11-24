67bd8f8e16e0853ee714fe6aa9a02eb8b2640bdb
beniroquai <bene.d@gmx.de>
2022-11-24 12:42:49 +0100

    Change folder structure

--- uc2docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/bene/Documents/UC2-DOCS/uc2docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/bene/Documents/UC2-DOCS/uc2docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/bene/Documents/UC2-DOCS/uc2docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/bene/Documents/UC2-DOCS/uc2docs/src/css/custom.css'),
-];

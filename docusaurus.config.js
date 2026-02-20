cb2b4825a82b16820b6dec05ab94d04b2e44ef8f
Ethan Li <lietk12@gmail.com>
2026-02-20 19:29:37 +0100

    Exclude archive & workshops from minimal Docker build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,6 +6,7 @@ const lightCodeTheme = themes.github;
 const darkCodeTheme = themes.dracula;
 
 const baseURL = process.env.BASE_URL || '/'
+const variant = process.env.VARIANT || 'full'
 
 /** @type {() => Promise<import('@docusaurus/types').Config>} */
 module.exports = async function createConfigAsync() {
@@ -65,7 +66,7 @@ module.exports = async function createConfigAsync() {
       ({
         docs: {
           routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve(`./sidebars-${variant}.js`),
           remarkPlugins: [math],
           rehypePlugins: [katex],
           // Please change this to your repo.
@@ -92,49 +93,7 @@ module.exports = async function createConfigAsync() {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      navbar: {
-        title: 'openUC2 Documentation',
-        logo: {
-          alt: 'openUC2 Logo',
-          src: 'img/Artboard4@4x.png',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'usage/README',
-            label: 'Usage',
-            position: 'left',
-          },
-          {
-            type: 'doc',
-            docId: 'dev/README',
-            label: 'Development',
-            position: 'left',
-          },
-          {
-            type: 'doc',
-            docId: 'workshops/README',
-            label: 'Workshops',
-            position: 'left',
-          },
-          {
-            type: 'doc',
-            docId: 'archive/README',
-            position: 'left',
-            label: 'Archive',
-          },
-          {
-            href: 'https://openuc2-gmbh.odoo.com',
-            label: 'Shop',
-            position: 'right',
-          },
-          {
-            href: 'https://github.com/openuc2/',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
+      navbar: require(`./navbar-${variant}.js`),
       algolia: {
         // The application ID provided by Algolia
         appId: 'DB3UCAMZ89',
